6e2848b72d5167365e819eab1ccfefbd93de1d94
Naman Goyal <namangoyaltms@outlook.com>
2025-01-17 12:30:29 +0530

    Update build configuration and clean up main.dart: add signing configs, enable resource shrinking, and remove old post deletion logic.

--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:huddle/app.dart';
 import 'package:huddle/common/config/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-Future<void> deleteOldPosts() async {
-  final firestore = FirebaseFirestore.instance;
-
-  final now = DateTime.now();
-  final sevenDaysAgo = now.subtract(Duration(days: 7));
-  final oldPosts = await firestore
-      .collection('posts')
-      .where('timestamp', isLessThan: sevenDaysAgo)
-      .get();
-
-  for (var doc in oldPosts.docs) {
-    await doc.reference.delete();
-  }
-
-  print('Deleted ${oldPosts.size} old posts');
-}
 
 void main() async {
-  // Flutter setup
+  //flutter setup
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Cache setup
+  //Cache setup
   await SharedPreferences.getInstance();
-
-  // Firebase setup
+  //firebase setup
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // Run cleanup for old posts
-  await deleteOldPosts();
 
   runApp(MyApp());
 }
+//Naman Goyal
