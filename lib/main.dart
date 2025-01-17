283dacad908351272597b62ee79379ba3b2c307b
Naman Goyal <namangoyaltms@outlook.com>
2025-01-17 11:55:20 +0530

    Add functionality to delete posts older than seven days from Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:huddle/app.dart';
 import 'package:huddle/common/config/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> deleteOldPosts() async {
+  final firestore = FirebaseFirestore.instance;
+
+  final now = DateTime.now();
+  final sevenDaysAgo = now.subtract(Duration(days: 7));
+  final oldPosts = await firestore
+      .collection('posts')
+      .where('timestamp', isLessThan: sevenDaysAgo)
+      .get();
+
+  for (var doc in oldPosts.docs) {
+    await doc.reference.delete();
+  }
+
+  print('Deleted ${oldPosts.size} old posts');
+}
 
 void main() async {
-  //flutter setup
+  // Flutter setup
   WidgetsFlutterBinding.ensureInitialized();
-  //Cache setup
+
+  // Cache setup
   await SharedPreferences.getInstance();
-  //firebase setup
+
+  // Firebase setup
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Run cleanup for old posts
+  await deleteOldPosts();
 
   runApp(MyApp());
 }
-//Naman Goyal
