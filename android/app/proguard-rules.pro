25db405aa5632e78e3a4b787fcefaad10dcfdcb7
Naman Goyal <namangoyaltms@outlook.com>
2025-01-18 22:34:27 +0530

    Refactor project structure: remove unused files, update application ID, and enhance Firebase initialization; add ProGuard rules for better code optimization.

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,12 @@
+# Keep the class names and member names for the app's entry point
+-keep class com.example.your_app.** { *; }
+
+# Keep class members annotated with @Keep
+-keepclassmembers class * {
+    @androidx.annotation.Keep *;
+}
+
+# Keep attributes related to annotations
+-keepattributes *Annotation*
+
+# Add rules to keep other necessary classes and methods as needed
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:huddle/app.dart';
 import 'package:huddle/common/config/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:huddle/app.dart';
 
 void main() async {
-  //flutter setup
   WidgetsFlutterBinding.ensureInitialized();
-  //Mobile ads setup
-  MobileAds.instance.initialize();
-  //Cache setup
-  await SharedPreferences.getInstance();
-  //firebase setup
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await _initializeFirebase();
   runApp(MyApp());
 }
-//Naman Goyal
+
+Future<void> _initializeFirebase() async {
+  try {
+    MobileAds.instance.initialize();
+    await SharedPreferences.getInstance();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  } catch (e) {
+    if (kDebugMode) {
+      print("Firebase initialization error: $e");
+    }
+  }
+}
